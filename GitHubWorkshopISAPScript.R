d0780a5f0e4e70ce0b4bef87baf9c04bed10ce4d
Rileigh Sandeman <Risandeman@gmail.com>
2025-11-20 13:45:22 -0500

    run model and summary of model

--- GitHubWorkshopISAPScript.R
+++ GitHubWorkshopISAPScript.R
@@ -2,8 +2,10 @@ library(readxl)
 #reading in excel puffin data
 Puffin <- read_excel("~/Desktop/StatsforBiol/Data/Puffin (1).xlsx")
 
+#print data
 head(puffin)
 
-
-
+#run summary of model
+model1 <- lm(sex~curlen,data=Puffin)
+summary(model1)
 
